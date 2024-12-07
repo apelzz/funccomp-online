7e373fe905cd521c80aab7897df677718a8d8f22
Lily Zihui Zhu <zhuzihui.lily@gmail.com>
2024-12-06 22:48:05 -0500

    added debrief

--- data_includes/main.js
+++ data_includes/main.js
@@ -27,6 +27,7 @@ Sequence(
     "Func2test",
     "FuncComp_intro",
     randomize("FuncComp"),
+    "debrief",
     "send_results",
     "completion_screen"
 );
@@ -34,25 +35,25 @@ Sequence(
 CheckPreloaded('Func1train', 120000).label("checkloadings");
 
 ////////////////////Text for the consent & instructions////////////////////
-consent_text = "<strong>Researcher:</strong> Lily Zhu, Graduate Student	<br><strong>Faculty Advisor: </strong> Dr. Jesse Snedeker	<br><strong>Contact information: </strong> (617) 496-7175] | kidlab@g.harvard.edu 	<br>	<br><strong>Key Information:</strong>	<br>The following is a short summary of this study to help you decide whether or not you want to participate. More detailed information is listed later on in this form. 	<br>	<br><strong> Why am I being invited to take part in a research study? </strong>	<br>We invite you to take part in this research study because you meet the study’s requirements and are over the age of 18.	<br> 	<br><strong>What should I know about being in a research study? </strong>	<ul>	<li>You'll receive instructions on how to participate.</li>	<li>Whether or not you take part is up to you.</li>	<li>Your participation is completely voluntary.</li>	<li>You can choose not to take part.</li>	<li>You can agree to take part and later change your mind.</li>	<li>Your decision will not be held against you.  </li>	</ul>		<br><strong> Why is this research being done? </strong>	<br>The purpose of this study is to better understand the processes involved in language comprehension and how they develop in adults and children. 		<br><strong> How long will the research last and what will I need to do? </strong>	<br>We expect that you will be in this research study for no longer than 30 minutes.	<br>During this study, you will be asked to watch a series of visual displays and interact with the displays in some way. You may have to respond to, identify, or judge various stimuli. 	<br>During the study, we will record your responses.		<br><strong>Is there any way being in this study could be bad for me? </strong>	<br>We don’t believe there are any risks from participating in this research. 		<br><strong> Will being in this study help me in any way? </strong>	<br>There are no benefits to you besides any enjoyment you may get from the task. There may be possible benefits to others by gaining more information about language comprehension and development; however, this depends on the outcomes of the study.<br>	<br><strong>Detailed Information:</strong><br>The following is more detailed information about this study.	<br><strong> What is the purpose of this research? </strong>	<br>The purpose of this research is to better understand the processes involved in language comprehension and how they develop in adults and children.		<br><strong>How long will I take part in this research? </strong>	<br>This study will take no longer than 30 minutes to complete. 		<br><strong>What can I expect if I take part in this research? </strong>	<br>This study will take place entirely online. During this study, you will be asked to watch a series of visual displays that included short video clips. You will be asked to interact with the displays by clicking on an objects. In addition to watching visual displays, you may hear/see some linguistic and/or non-linguistic stimuli (for example words or sentences) before, during, or after the display is presented.		<br><strong> What happens if I say yes, but I change my mind later? </strong>	<br>You can leave the research at any time it will not be held against you. 		<br><strong> If I take part in this research, how will my privacy be protected? What happens to the information you collect? </strong>	<br>Efforts will be made to limit the use and disclosure of your Personal Information, including research study and medical records, to people who have a need to review this information. We cannot promise complete secrecy. Organizations that may inspect and copy your information include the Institutional Review Board (IRB) and other representatives of this organization. 		<br>If any publication results from this research, you will not be identified by name. If identifiers are removed from your identifiable private information or identifiable samples that are collected during this research, that information or those samples could be used for future research studies or distributed to another investigator for future research studies without your additional informed consent.		<br><strong> What else do I need to know? </strong>	<br>Compensation - If you agree to take part in this research study, we will pay you at a rate of $15 hour.	<br>This research is being funded by the Stimson Fund from Harvard Psychology.		<br><strong> Who can I talk to? </strong>	<br>If you have questions, concerns, or complaints, or think the research has hurt you, contact the researcher at lilyzhu@fas.harvard.edu, the research team at kidlab@g.harvard.edu or (617) 496-2847, or the faculty member supervising this work: Prof Jesse Snedeker, snedeker@wjh.harvard.edu. 		<br>This research has been reviewed and approved by the Harvard University Area Institutional Review Board (“IRB”). You may talk to them at (617) 496-2847 or cuhs@harvard.edu if: 	<ul>	<li> Your questions, concerns, or complaints are not being answered by the research team. </li>	<li> You cannot reach the research team. </li>	<li> You want to talk to someone besides the research team. </li>	<li> You have questions about your rights as a research subject. </li>	<li> You want to get information or provide input about this research. </li></ul>";	
+consent_text = "<strong>Researcher:</strong> Lily Zihui Zhu, Graduate Student	<br><strong>Faculty Advisor: </strong> Dr. Jesse Snedeker	<br><strong>Contact information: </strong> (617) 496-7175] | kidlab@g.harvard.edu 	<br>	<br><strong>Key Information:</strong>	<br>The following is a short summary of this study to help you decide whether or not you want to participate. More detailed information is listed later on in this form. 	<br>	<br><strong> Why am I being invited to take part in a research study? </strong>	<br>We invite you to take part in this research study because you meet the study’s requirements and are over the age of 18.	<br> 	<br><strong>What should I know about being in a research study? </strong>	<ul>	<li>You'll receive instructions on how to participate.</li>	<li>Whether or not you take part is up to you.</li>	<li>Your participation is completely voluntary.</li>	<li>You can choose not to take part.</li>	<li>You can agree to take part and later change your mind.</li>	<li>Your decision will not be held against you.  </li>	</ul>		<br><strong> Why is this research being done? </strong>	<br>The purpose of this study is to better understand the processes involved in language comprehension and how they develop in adults and children. 		<br><strong> How long will the research last and what will I need to do? </strong>	<br>We expect that you will be in this research study for no longer than 30 minutes.	<br>During this study, you will be asked to watch a series of visual displays and interact with the displays in some way. You may have to respond to, identify, or judge various stimuli. 	<br>During the study, we will record your responses.		<br><strong>Is there any way being in this study could be bad for me? </strong>	<br>We don’t believe there are any risks from participating in this research. 		<br><strong> Will being in this study help me in any way? </strong>	<br>There are no benefits to you besides any enjoyment you may get from the task. There may be possible benefits to others by gaining more information about language comprehension and development; however, this depends on the outcomes of the study.<br>	<br><strong>Detailed Information:</strong><br>The following is more detailed information about this study.	<br><strong> What is the purpose of this research? </strong>	<br>The purpose of this research is to better understand the processes involved in language comprehension and how they develop in adults and children.		<br><strong>How long will I take part in this research? </strong>	<br>This study will take no longer than 30 minutes to complete. 		<br><strong>What can I expect if I take part in this research? </strong>	<br>This study will take place entirely online. During this study, you will be asked to watch a series of visual displays that included short video clips. You will be asked to interact with the displays by clicking on an objects. In addition to watching visual displays, you may hear/see some linguistic and/or non-linguistic stimuli (for example words or sentences) before, during, or after the display is presented.		<br><strong> What happens if I say yes, but I change my mind later? </strong>	<br>You can leave the research at any time it will not be held against you. 		<br><strong> If I take part in this research, how will my privacy be protected? What happens to the information you collect? </strong>	<br>Efforts will be made to limit the use and disclosure of your Personal Information, including research study and medical records, to people who have a need to review this information. We cannot promise complete secrecy. Organizations that may inspect and copy your information include the Institutional Review Board (IRB) and other representatives of this organization. 		<br>If any publication results from this research, you will not be identified by name. If identifiers are removed from your identifiable private information or identifiable samples that are collected during this research, that information or those samples could be used for future research studies or distributed to another investigator for future research studies without your additional informed consent.		<br><strong> What else do I need to know? </strong>	<br>Compensation - If you agree to take part in this research study, we will pay you at a rate of $15 hour.	<br>This research is being funded by the Stimson Fund from Harvard Psychology.		<br><strong> Who can I talk to? </strong>	<br>If you have questions, concerns, or complaints, or think the research has hurt you, contact the researcher at lilyzhu@fas.harvard.edu, the research team at kidlab@g.harvard.edu or (617) 496-2847, or the faculty member supervising this work: Prof Jesse Snedeker, snedeker@wjh.harvard.edu. 		<br>This research has been reviewed and approved by the Harvard University Area Institutional Review Board (“IRB”). You may talk to them at (617) 496-2847 or cuhs@harvard.edu if: 	<ul>	<li> Your questions, concerns, or complaints are not being answered by the research team. </li>	<li> You cannot reach the research team. </li>	<li> You want to talk to someone besides the research team. </li>	<li> You have questions about your rights as a research subject. </li>	<li> You want to get information or provide input about this research. </li></ul>";	
 
 read_consent = "Before we start the study, you’ll need to read the following information and consent to participating. Go ahead and read the consent form below.";
 
 general_task_description = "In this study, you will watch some videos and view some images. Based on the prompts and the videos, you will need to click on specific images.";
-
 practice_instr = "Here's an example: You'll see an object after the screen is lifted.";
 
 train_block_instr = "In the next few videos, you'll see an object appear when the screen is lifted. Pay close attention—something might happen to it!";
-
 train_trial_instr = "Look, something might happen to the object!";
 
 singleFuncTest_block_instr = "In the next few videos, you'll first see an object. Then, when the screen falls and covers it, something will happen to the object. Your job is to guess what the object is like afterwards!";
-
 singleFuncTest_trial_instr = "Can you guess what the object will be like at the end?";
 
 funcCompTest_block_instr = "Alright! Now that you have an idea of what might happen to the object behind the screen, let’s try it some more. The task is the same: Guess what the object will be like after something happens to it behind the screen!";
+funcCompTest_trial_instr = "What do you think the object will look like at the end?";
 
-funcCompTest_trial_instr = "What do you think the object will look like at the end?"
+debrief1 = "Let me tell you a little more about why we’re doing this study!";
+debriefextended = "Dr. Jesse Snedeker <br>	Lily Zihui Zhu <br>	<br>	<strong>Debriefing for \"Function Composition\" </strong><br>	<br>  	Thank you for participating in our study. This study examines how people systematically combine concepts in the domain of physical reasoning. Participants watched videos depicting two physical functions: pushing and smashing, which could change an object’s location and state, respectively. At test, the target object appeared at different locations, and the functions were applied in varying sequences. The outcomes depended on the starting state and the order of events. For example, if the object began on the right and smashing occurred first on the left, followed by pushing it to the left, the object remained intact. However, reversing the order—pushing before smashing—resulted in the object being smashed. Success required not only representing both functions but also understanding that the order of application could alter the intermediate context, thereby affecting the final outcome.  <br>		<u>How to contact the researcher:</u> If you have questions or concerns about your participation or payment, or want to request a summary of research findings, please contact the researcher: (<strong>Lily Zihui Zhu</strong>, lilyzhu@fas.harvard.edu). For any other problems related to this study, you may also contact the faculty member supervising this work: (<strong>Jesse Snedeker</strong>, snedeker@wjh.harvard.edu). <br>		<u> Whom to contact about your rights in this research </u>: For questions, concerns, suggestions, or complaints that are not being addressed by the researcher, or research-related harm: Committee on the Use of Human Subjects in Research at Harvard University, Phone: 617-496-CUHS (2847). Email: cuhs@harvard.edu <br>";
+
 
 //////////////////////////// Header ////////////////////////////
 Header(
@@ -282,7 +283,7 @@ newTrial("consent", // Collect consent
 )
 .setOption("countsForProgressBar", false)
 
-newTrial( "soundcheck" , 
+newTrial("soundcheck" , 
     newText("sound_checkdirections", "Click below to listen to the directions for the sound check!")
         .center()
         .print()
@@ -487,7 +488,7 @@ Template("adult_ordersheet_long_Func1train.csv", row =>
             .play("once")
             .wait("first")
         ,
-        newTimer("autoplayDelay", 500).start().wait()
+        newTimer("autoplayDelay", 10).start().wait()
         ,
         getVideo("train_video")
             .play()
@@ -552,7 +553,7 @@ Template("adult_ordersheet_long_Func1test.csv", row =>
             .play("once")
             .wait("play")
         ,
-        newTimer("autoplayDelay", 500).start().wait()
+        newTimer("autoplayDelay", 10).start().wait()
         ,
         getVideo("singleFuncTest_video")
             .play()
@@ -677,7 +678,7 @@ Template("adult_ordersheet_long_Func2train.csv", row =>
             .play("once")
             .wait("first")
         ,
-        newTimer("autoplayDelay", 500).start().wait() // 1 second delay
+        newTimer("autoplayDelay", 10).start().wait() // 1 second delay
         ,
         getVideo("train_video")
             .play()
@@ -742,7 +743,7 @@ Template("adult_ordersheet_long_Func2test.csv", row =>
             .play("once")
             .wait("play")
         ,
-        newTimer("autoplayDelay", 500).start().wait()
+        newTimer("autoplayDelay", 10).start().wait()
         ,
         getVideo("singleFuncTest_video")
             .play()
@@ -869,7 +870,7 @@ Template("adult_ordersheet_long_FuncComp.csv", row =>
             .play("once")
             .wait("first")
         ,
-        newTimer("autoplayDelay", 500).start().wait()
+        newTimer("autoplayDelay", 10).start().wait()
         ,
         getVideo("funcCompTest_video")
             .play()
@@ -921,10 +922,25 @@ Template("adult_ordersheet_long_FuncComp.csv", row =>
     .log("distractor_image3", row.distractor3)
 )
 
-// Send results manually
+//////////////Debriefing//////////////
+newTrial("debrief",
+    newText("deb1",debrief1).center()
+    ,
+    //newText("deb2",debrief2).center()
+    //,
+    newText("extendedDeb", debriefextended)
+        .css({"text-align": "left",
+             'font-size':'1em'})
+        .print()
+    ,
+    newButton("enddebrief", "Got it!").print().wait()
+    
+    );
+
+//////////////Send results manually//////////////
 SendResults("send_results")
 
-// Completion screen
+//////////////Completion screen//////////////
 newTrial("completion_screen",
     newText("thanks", "Thank you for participating! You may now exit the window.")
         .center()
